d8a0497109e16f7526b81872cb47c93cdff9c3df
cucumberhands <cyrus.simons@yahoo.com>
2019-01-21 20:45:33 -0500

    customized website base and created navbar, message display, etc.

--- loginproject/settings.py
+++ loginproject/settings.py
@@ -77,6 +77,10 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 WSGI_APPLICATION = 'loginproject.wsgi.application'
 
 
@@ -128,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'home'
